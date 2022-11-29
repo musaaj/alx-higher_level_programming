e8db294f12a718392645ce3ecafe377c6caadd6d
= <=>
2022-11-29 02:38:47 +0100

    Auto Commit

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,16 +6,16 @@
  * @list: head of a linked list
  * Return: 1 if there is cycle else 0
  */
-int check_cycle(list_t *list)
+int check_cycle(listint_t *list)
 {
-	list_t p1, p2;
+	listint_t p1, p2;
 
 	if (list)
 		return (0);
-	p1 = malloc(sizeof(list_t));
+	p1 = malloc(sizeof(listint_t));
 	if (!p1)
 		return (0);
-	p2 = malloc(sizeof(list_t));
+	p2 = malloc(sizeof(listint_t));
 	if (!p2)
 	{
 		free(p1);
