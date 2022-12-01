1c24a4bda71e1b91431cc2df0b4e8c23721d3192
= <=>
2022-12-01 04:05:34 +0100

    Auto Commit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-chars = ""
-
 for c in range(ord('a'), ord('{')):
     if ((chr(c) != 'q') and (chr(c) != 'e')):
-        chars += chr(c)
-print(chars, end='')
+        print("{0}".format(chr(c)), end='')
