1b2c855f5613aa7f31125f878f1f23e32e507c03
= <=>
2022-12-03 12:08:49 +0100

    Auto Commit

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * insert_node - insert an int into a listint object
+ * @head: head node of a listint object
+ * @number: int to be inserted
+ * Return: address of newly inserted node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node;
+
+	node = malloc(sizeof(listint_t));
+	if (!node)
+		return (NULL);
+	if (!head)
+		return (NULL);
+	node->n = number;
+	while (*head)
+	{
+		if ((*head)->n > number)
+		{
+			node->next = *head;
+			node = *head
+		}
+		head++;
+	}
+	return (node);
+}
