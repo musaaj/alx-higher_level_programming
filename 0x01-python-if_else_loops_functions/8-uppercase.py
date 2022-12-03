ec7de8ced96c93bcd5c90c21dda4dec9800cb7f9
= <=>
2022-12-03 08:20:03 +0100

    Auto Commit

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    tmp: str = ""
+    code: int = 32
+    for c in str:
+        if ((ord(c) >= ord('a')) and (ord(c) <= ord('z'))):
+            tmp += chr(ord(c) - code)
+        else:
+            tmp += c
+    print(tmp)
