7022fe2955442d898b0d83c78acec0cd2f69af8c
= <=>
2022-12-03 07:57:11 +0100

    Auto Commit

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+
+for i in range(0, 10):
+    for j in range(0, 10):
+        if (i < j):
+            if ((i == 8) and (j == 9)):
+                print(f"{i}{j}")
+            else:
+                print(f"{i}{j}, ", end="")
