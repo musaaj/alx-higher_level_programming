9935f88f1a95890ab43b9e546f104d662d3f58d5
= <=>
2022-12-03 08:56:03 +0100

    Auto Commit

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for n in range(1, 101):
+        if ((n % 5 == 0) and (n % 3 == 0)):
+            print("FizzBuzz ", end="")
+        elif (n % 5 == 0):
+            print("Buzz ", end="")
+        elif (n % 3):
+            print("Fizz ", end="")
+        else:
+            print("{0}".format(n), end="")
