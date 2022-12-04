856bdae8107cc879f5e28efd08cdf564cebd107e
Musa Ibrahim <=>
2022-12-04 20:14:04 +0100

    Auto Commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+if (__name__ == '__main__'):
+    from sys import argv
+
+    arg_len = len(argv)
+    info = 'argument'
+    i: int = 1
+
+    if (arg_len < 2):
+        info += '.'
+    if (arg_len == 2):
+        info += ':'
+    if (arg_len > 2):
+        info += 's:'
+    print('{:d} {:s}'.format(arg_len - i, info))
+    while (i < arg_len):
+        print('{:d}: {:s}'.format(i, argv[i]))
+        i += 1
