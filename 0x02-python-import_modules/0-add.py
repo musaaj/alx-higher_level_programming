658e217c4b58d66126eb4aae366f0ba4a53520ec
Musa Ibrahim <=>
2022-12-04 05:57:41 +0100

    Auto Commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import add_0 as add
+import add_0
 
 
 def main():
     a = 1
     b = 2
 
-    print("<a {0}> + <b {1}> = <add(a, b) {2}>".format(a, b, add.add(a, b)))
+    print("<a {0}> + <b {1}> = <add(a, b) {2}>".format(a, b, add_0.add(a, b)))
 
 
 if (__name__ == "__main__"):
