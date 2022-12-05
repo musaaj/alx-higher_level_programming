739973130e90232a778cf6d18a9e40849fa40ece
= <=>
2022-12-05 02:15:22 +0100

    Auto Commit

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if (my_list is None):
-        return (my_list)
-    if ((idx < 0) or (idx >= len(element))):
-        return (my_list)
+        return (None)
     tmp = [item for item in my_list]
+    if ((idx < 0) or (idx >= len(element))):
+        return (tmp) 
     tmp[idx] = element
     return (tmp)
 
