3870ddb8c78164c708c3b5f17866e53385c879a0
= <=>
2022-12-05 03:13:37 +0100

    Auto Commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for matrice in matrix:
-        for i in matrice:
-            print('{:d} '.format(i), end='')
+    for rows in matrix:
+        for col in rows:
+            print('{:d} '.format(col), end='')
         print('')
