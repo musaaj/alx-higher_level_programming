dc0736614cc0c8f6401c5cd1fcfdc864c20fbac1
= <=>
2022-12-05 16:36:40 +0100

    Auto Commit

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-import sys
-
-
 def max_integer(my_list=[]):
-    my_max = -sys.maxint - 1
+    my_max = 0
     if ((my_list is None) or (len(my_list) == 0)):
         return (None)
+    my_max = my_list[0]
     for i in my_list:
         my_max = i if i > my_max else my_max
     return (i)
