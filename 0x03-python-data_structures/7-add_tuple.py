f2336e9a1c33025b0cc4da7f67fd4fa71a2a6042
= <=>
2022-12-05 16:07:05 +0100

    Auto Commit

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a:int = 0
+    b:int = 0
+
+    if (len(tuple_a) >= 1):
+        a = tuple_a[0]
+    if (len(tuple_a) >= 2):
+        b = tuple_a[1]
+    if (len(tuple_b) >= 1):
+        a += tuple_b[0]
+    if (len(tuple_b) >= 2):
+        b += tuple_b[1]
+    return (a, b)
