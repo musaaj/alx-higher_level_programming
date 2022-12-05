3fd203ae11f5375de5658637bbd86f97c15c962f
= <=>
2022-12-05 01:44:07 +0100

    Auto Commit

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -11,7 +11,7 @@ def element_at(my_list, idx):
         if idx < 0 None
         if my_list is None None
     """
-    if (idx < 1):
+    if (idx < 0):
         return (None)
     if (idx >= len(my_list)):
         return (None)
