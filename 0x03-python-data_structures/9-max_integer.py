4b3f6d41c951fa8613bd84d521ddd5ee0d22055e
= <=>
2022-12-05 16:27:37 +0100

    Auto Commit

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,5 +4,5 @@ def max_integer(my_list=[]):
     if ((my_list is None) or (len(my_list) == 0)):
         return (None)
     for i in my_list:
-        my_max = i if i > my_max
+        my_max = i if i > my_max else my_max
     return (i)
