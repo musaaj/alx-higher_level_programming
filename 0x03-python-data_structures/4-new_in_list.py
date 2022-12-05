75363feb5a394de0340176c49371adfac4a6bbbc
= <=>
2022-12-05 02:32:57 +0100

    Auto Commit

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     if (my_list is None):
         return (None)
     tmp = [item for item in my_list]
-    if ((idx < 0) or (idx >= len(element))):
+    if ((idx < 0) or (idx >= len(my_list))):
         return (tmp) 
     tmp[idx] = element
     return (tmp)
