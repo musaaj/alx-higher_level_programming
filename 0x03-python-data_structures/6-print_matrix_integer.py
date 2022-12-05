3fd85f55514f5efa7c3b6814035742b7cb77b887
= <=>
2022-12-05 03:15:22 +0100

    Auto Commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if (matrix is None):
+        return
     for rows in matrix:
         for col in rows:
             print('{:d} '.format(col), end='')
