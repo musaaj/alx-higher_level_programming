80ef89a46e5941730f7556bc2a2186a7cbca2c14
= <=>
2022-12-05 02:34:31 +0100

    Auto Commit

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,7 +4,6 @@ def new_in_list(my_list, idx, element):
         return (None)
     tmp = [item for item in my_list]
     if ((idx < 0) or (idx >= len(my_list))):
-        return (tmp) 
+        return (tmp)
     tmp[idx] = element
     return (tmp)
-
