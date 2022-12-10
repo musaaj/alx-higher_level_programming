04d9d67e90621e019761df8e55dc0590f3c630e5
Musa Ibrahim <=>
2022-12-10 22:51:22 +0100

    Auto Commit

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    result = [replace for i in my_list if i == search else i]
+    result = [i for i in my_list if i != search]
     return result
