12bb12e52c759baaa5f80fe8a5208a601e01f7ed
Musa Ibrahim <=>
2022-12-10 22:13:51 +0100

    Auto Commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    result = [row for row in matrix col**2 for col in row]
+    result = [[col**2 for col in row] for row in matrix]
     return result
