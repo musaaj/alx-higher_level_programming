d2cd9fdca9e3e662cb0f6b183e277ad30b3a8d15
Musa Ibrahim <=>
2022-12-10 18:41:39 +0100

    Auto Commit

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,5 +3,5 @@
 def delete_at(my_list=[], idx=0):
     if (idx < 0 or idx > len(my_list) or len(my_list) == 0):
         return my_list
-    del(my_list[idx])
+    del (my_list[idx])
     return my_list
