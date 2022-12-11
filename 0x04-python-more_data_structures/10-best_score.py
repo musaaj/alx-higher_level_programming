1ffb18b76d7d765defc2b5b3a8fb89295ae9920c
= <=>
2022-12-11 16:12:29 +0100

    Auto Commit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,6 +4,10 @@ def best_score(a_dictionary):
     best_key = None
     best_score = 0
 
+    if (a_dictionary is None)
+        return None
+    if (len(a_dictionary) == 0)
+        return None
     for (key, value) in a_dictionary.items():
         if (value > best_score):
             best_key = key
