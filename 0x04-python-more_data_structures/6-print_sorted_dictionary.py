bfbb0b2d59c25c2d1894a87b2b581963d8909746
Musa Ibrahim <=>
2022-12-11 12:55:12 +0100

    Auto Commit

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    return dict(sorted(a_dictionary.items()))
+    my_dict = dict(sorted(a_dictionary.items()))
+    for key, item in my_dict:
+        print("{0}: {1}".format(key, item))
