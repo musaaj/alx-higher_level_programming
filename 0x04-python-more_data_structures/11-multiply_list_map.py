fc314f0371e2cac121d55cd184c060be5aa93428
= <=>
2022-12-11 16:26:44 +0100

    Auto Commit

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def multiply_list_map(my_list=[], number=0):
-    tmp = dict(map(my_list.items(), lambda key, val: (key, val * number)))
+    tmp = list(map(my_list, lambda x: x * number))
     return tmp
