0ca3e37eeae97a4b58ba982a3b2f41821613f51d
= <=>
2022-12-11 16:00:33 +0100

    Auto Commit

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    tmp = {key: val * 2 for key, val in a_dictionary}
+    tmp = {key: val * 2 for (key, val) in a_dictionary.items()}
     return tmp
