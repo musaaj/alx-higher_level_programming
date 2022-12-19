e4f097db93f2561ecae4b2afebc38c978f60d3b4
= <=>
2022-12-19 12:00:22 +0100

    Auto Commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    while (i < x):
+        try:
+            print("{}".format(my_list[i]), end="")
+        except IndexError:
+            print()
+            return i
+        i += 1
+    print()
+    return i
+
