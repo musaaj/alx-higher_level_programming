9cb6cf7915c40b449aac8b5c45c0c903763ef4b9
Musa Ibrahim <=>
2022-12-20 17:15:46 +0100

    Auto Commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""Simple python module that does
+absolutely nothing
+
+This is just another paragraph
+"""
+
+
+class Square:
+    """Class declaration example
+
+    Simple class docstring sample
+    """
+
+    def __init__(self, size):
+        """constructor method of our class
+
+    This function initiate our class instance
+
+         Args:
+            size (int): size of our square object
+        """
+        self.__size = size
