8069ddb3f1a291a19f29ba5655a17b39d445c01a
Musa Ibrahim <=>
2022-12-20 17:46:47 +0100

    Auto Commit

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""Simple python module that does
+absolutely nothing
+
+This is just another paragraph
+"""
+
+
+class Square(object):
+    """Class declaration example
+
+    Simple class docstring sample
+    """
+
+    def __init__(self, size=0):
+        """constructor method of our class
+
+    This function initiate our class instance
+
+         Args:
+            size (int): size of our square object
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """find area of a square instance
+
+        Calculate the area of the square instance
+
+        Returns:
+            int: The area of the square
+        """
+        return self.__size**2
