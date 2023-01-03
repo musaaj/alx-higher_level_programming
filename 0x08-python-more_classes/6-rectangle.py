42b58de080051b315e7ccffc6454177e215e66a8
Musa Ibrahim <musaaj@gmail.com>
2023-01-03 21:35:06 +0100

    auto commit

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -26,7 +26,7 @@ class Rectangle:
             raise ValueError('height must be >= 0')
         self.width = width
         self.height = height
-        number_of_instances += 1
+        self.number_of_instances += 1
 
     def __str__(self):
         """Returns printed instance of Rectangle"""
