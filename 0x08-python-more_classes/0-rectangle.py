8025362dd3659a2eaa522cf5650e606eb1bac571
Musa Ibrahim <musaaj@gmail.com>
2023-01-03 19:17:42 +0100

    auto commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Example of module in python
+"""
+
+
+class Rectangle:
+    """Example of how to declare a class"""
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""Simple Module"""
+
+
+class Rectangle:
+    """Simple Rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """constructor method
+        Args:
+            width: must be an unsigned int
+            height: must be an unsigned int
+        """
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+        if width < 0:
+            raise ValueError('width must be >= 0')
+        if type(heigt) is not int:
+            raise TypeError('height must be an integer')
+        if height < 0:
+            raise ValueError('height must be >= 0')
+        self._width = width
+        self._height = height;
+
+    @property
+    def width(self):
+        """width getter
+        Return: unsigned int
+        """
+        return self._width
+
+    @property
+    def height(self):
+        """height getter
+        Return: unsigned int
+        """
+        return self._height
+
+    @width.setter
+    def width(self, value):
+        """width setter
+        Args:
+            value: unsigned int
+        """
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+        if width < 0:
+            raise ValueError('width must be >= 0')
+        self._width = value
+
+    @height.setter
+    def height(self, value):
+        """height getter
+        Args:
+            value: unsigned int
+        """
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self._height = value
