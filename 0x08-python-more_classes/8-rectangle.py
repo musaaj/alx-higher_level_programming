c52d104df8024b5409e2fb52ff6382840e3e6eb9
Musa Ibrahim <musaaj@gmail.com>
2023-01-03 22:56:37 +0100

    auto commit

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -114,7 +114,10 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        """returns the bigger rectangle"""
+        """returns the bigger rectangle
+
+        Return: Rectangle instance
+        """
         if not isintance(rect_1, Rectangle):
             raise TypeError('rect_1 must be an instance of Rectangle')
         if not isintance(rect_2, Rectangle):
