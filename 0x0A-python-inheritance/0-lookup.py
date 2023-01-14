2ab84694a65fb5107c8457bb2192df8209a40a8d
Musa Ibrahim <musaaj@gmail.com>
2023-01-14 20:19:37 +0100

    Auto Commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Lookup module"""
+
+
+def lookup(obj):
+    """check for object attributes
+
+    Args:
+        obj: any
+
+    Return: list of @obj attributes
+    """
+    return sorted(obj.__dir__())
