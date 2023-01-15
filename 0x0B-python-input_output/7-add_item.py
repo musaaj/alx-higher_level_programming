6649fd9143967f3f3b719134b14ee30674f033bf
Musa Ibrahim <musaaj@gmail.com>
2023-01-15 22:35:35 +0100

    Auto Commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """append json"""
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-import sys
 
 
 def main():
@@ -17,6 +17,7 @@ def main():
     my_list = load_from_json_file('add_item.json')
     my_list.extend(sys.argv[1:])
     save_to_json_file(my_list, 'add_item.json')
+
 
 if __name__ == '__main__':
     main()
