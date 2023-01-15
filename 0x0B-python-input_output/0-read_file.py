64fa55e1b7730f8152989f3f860d3a85281b70b5
Musa Ibrahim <musaaj@gmail.com>
2023-01-15 15:13:49 +0100

    Auto Commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Read text file"""
+
+
+def read_file(filename=""):
+    """Read and print text file to stdout
+
+    Args:
+        filename: string
+    """
+    with open(filename, 'r') as fp:
+        for line in fp:
+            print('{}'.format(line))
