183d08859540663a0ad8d9c96a55ec699a4438d8
Musa Ibrahim <musaaj@gmail.com>
2023-01-15 15:39:09 +0100

    Auto Commit

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3
+"""write or append to text file"""
+
+
+def append_write(filename="", text=""):
+    """write or append text file
+
+    Args:
+        filename: string
+        text: string
+
+    Return: int number of char written
+    """
+    with open(filename, 'w+') as fp:
+        return fp.write(text)
