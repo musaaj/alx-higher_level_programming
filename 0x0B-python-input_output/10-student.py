ba2b78a4dd3df14f00614b9875454e8b1b13c5c3
Musa Ibrahim <musaaj@gmail.com>
2023-01-31 15:35:01 +0100

    Auto Commit

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Student module
+"""
+
+
+class Student:
+    """
+    A simple student class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """constructor method
+
+        Args:
+            first_name: str
+            last_name str
+            age: int
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """get dictionary reps of all fields of
+        instance of this class
+
+        Args:
+            attrs: list of strings
+
+        Return: dictionary reps of self
+        """
+        if not attrs:
+            return vars(self)
+        object_attrs = {}
+        attributes = vars(self)
+        for field in attrs:
+            object_attrs[field] = attributes[field]
+        return object_attrs
