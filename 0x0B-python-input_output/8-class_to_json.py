334cf199148dec045a3d125c84749e3294f50064
Musa Ibrahim <musaaj@gmail.com>
2023-01-31 15:12:09 +0100

    Auto Commit

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Object to json
+"""
+def class_to_json(obj):
+    """returns dictionary reps of an object fields
+
+    Args:
+        obj: any none primitive object 
+
+    Return: dict, dictionary reps of @obj
+    """
+    return vars(obj)
