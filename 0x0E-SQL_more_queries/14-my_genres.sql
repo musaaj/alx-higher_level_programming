50e12e7a9b96fe5082fa2e5fa665c08275a7217a
Musa Ibrahim <musaaj@gmail.com>
2023-02-16 19:37:27 +0100

    Auto Commit

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,13 @@
+-- lists all genres of the show Dexter.
+SELECT
+    a.name
+FROM
+    tv_genres a
+    INNER JOIN tv_show_genres b ON b.genre_id = a.id
+    INNER JOIN tv_shows c ON c.id = b.show_id
+WHERE
+    c.title = "Dexter"
+GROUP BY
+    a.name
+ORDER BY
+    a.name ASC;
