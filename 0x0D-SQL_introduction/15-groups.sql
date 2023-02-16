f31be0cee43ac2c9f9e37a3f2baa494c95f69d63
Musa Ibrahim <musaaj@gmail.com>
2023-02-16 22:50:33 +0100

    Auto Commit

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,12 @@
 -- count distinct
-SELECT DISTINCT score COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number
+-- SELECT DISTINCT score COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number
+SELECT
+    score,
+    COUNT(*) AS number
+FROM
+    second_table
+GROUP BY
+    score
+ORDER BY
+    score DESC,
+    number;
