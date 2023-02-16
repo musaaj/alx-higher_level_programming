bcd006e7d829297491150e8e4e10ef90dc405492
Musa Ibrahim <musaaj@gmail.com>
2023-02-16 18:50:55 +0100

    Auto Commit

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,13 @@
+-- creates a database and a new user
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+
+SET
+    PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
+
+GRANT
+SELECT
+    ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+FLUSH PRIVILEGES;
